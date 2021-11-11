68a986d4c2e44f74f71313ee2719df114fbed910
Fayaz Shaik <fayazbasha974@gmail.com>
2021-11-11 13:38:11 +0530

    webinars list api integrated

--- app/src/app/users/webinars-list/webinars-list.component.ts
+++ app/src/app/users/webinars-list/webinars-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-webinars-list',
@@ -8,13 +9,36 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class WebinarsListComponent implements OnInit {
 
-  constructor(private readonly activatedRoute: ActivatedRoute) { }
+  webinars: any = [];
+  webinarPriceType: string = '';
+
+  constructor(private readonly activatedRoute: ActivatedRoute,
+    private readonly usersService: UsersService,
+    private readonly router: Router) { }
 
   ngOnInit(): void {
     this.activatedRoute.queryParamMap.subscribe(value => {
-      console.log(value);
+      console.log(value.get('type'));
+      const webinarType = value.get('type') || '';
+      this.getUpcomingWebinars(webinarType);
     })
+    
     console.log('called');
+  }
+
+  getUpcomingWebinars(type: string) {
+    const params = `webinarType/${type}`;
+    this.usersService.getWebinars(params).subscribe((response: any) => {
+      console.log(response);
+      this.webinars = response;
+      this.webinarPriceType = type === 'ondemand' ? 'recOneAttendeePrice': 'liveOneAttendeePrice';
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  viewDetails(webinar: any) {
+    this.router.navigate(['/users/webinar-detail'], {queryParams: {title: webinar.title, id: webinar.id}})
   }
 
 }
